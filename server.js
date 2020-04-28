1d69ee06095c9d605aaacc6fdbe74d5b8896b397
Hamza Yousaf <Hamzayousaf933@gmail.com>
2020-04-28 21:36:22 +0500

    Sign-up & sign-in & auth middleware

--- server.js
+++ server.js
@@ -4,17 +4,24 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
 const fileupload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
+const auth = require('./routes/auth');
 
 dotenv.config({ path: './config/config.env' });
 
 connectDB();
 
 const app = express();
+
+// Body parser
 app.use(express.json());
+
+// Cookie parser
+app.use(cookieParser());
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -29,6 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
+app.use('/api/v1/auth', auth);
 
 app.use(errorHandler);
 
